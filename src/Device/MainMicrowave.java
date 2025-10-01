db54c0afbe8d10e2945bafd3e089622fd92ecfd7
michelle-annmiclat <michelle-ann.miclat.567@my.csun.edu>
2025-10-01 14:54:18 -0700

    Update MainMicrowave.java

--- src/Device/MainMicrowave.java
+++ src/Device/MainMicrowave.java
@@ -1,4 +1,9 @@
 package Device;
 
 public class MainMicrowave {
+    public static void main(String[] args) {
+        Microwave myMicrowave = new Microwave(1200, 30); //
+        myMicrowave.printDescription();
+
+    }
 }
