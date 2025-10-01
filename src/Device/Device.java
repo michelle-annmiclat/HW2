d06a972b27ac3683258873bd8cfcbe4e6852cafc
michelle-annmiclat <michelle-ann.miclat.567@my.csun.edu>
2025-10-01 14:53:59 -0700

    Update Device.java

--- src/Device/Device.java
+++ src/Device/Device.java
@@ -1,4 +1,21 @@
 package Device;
 
 public class Device {
+    public String model = "Generic Device";
+    protected int powerLevel; // Protected so subclasses can access it
+
+    public Device(int startPower) {
+        this.powerLevel = startPower;
+        System.out.println("--- Device initialized. ---");
+    }
+
+    public void setPower(int newLevel) {
+        if (newLevel >= 0) {
+            powerLevel = newLevel;
+        }
+    }
+
+    public void printDescription() {
+        System.out.println("Device Model: " + model + ", Current Power: " + powerLevel + " Watts.");
+    }
 }
